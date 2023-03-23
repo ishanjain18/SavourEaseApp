6966b2d03a733245765a79c95efada6666db0604
Ishan <ishan10j@gmail.com>
2023-03-24 01:43:06 +0530

    updated files

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import numpy as np
 from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -11,15 +13,17 @@ class Recipe(BaseModel):
 
 class FormInput(BaseModel):
     ingredients: List[str] = []
-    total_time: int
-    servings: int
+    total_time: int = 0
+    servings: int = 0
     course: str = ""
     diet: str = ""
+    cuisine: str = ""
     
 
 app = FastAPI()
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv', encoding='utf-8')
+encoded_df = pd.read_csv('encoded.csv')
 
 app.add_middleware(
     CORSMiddleware,
@@ -42,8 +46,8 @@ def get_ingredients():
             unique_ingredients[ingred] = unique_ingredients.get(ingred, 0) + 1
     
     # filtering ingredients based on frequency in dataset 
-    # min_freq = 15
-    # unique_ingredients = dict(filter(lambda x : x[1] > min_freq, unique_ingredients.items()))
+    min_freq = 15
+    unique_ingredients = dict(filter(lambda x : x[1] > min_freq, unique_ingredients.items()))
 
     return {"ingredients": unique_ingredients}
 
@@ -54,7 +58,56 @@ def get_suggestions():
 
 @app.get('/recommendations')
 def get_recommendations(form: FormInput):
-    return form
+
+    # build form input into a encoded vector
+
+    # 1. by similar dish 
+    # 2. by course 
+    # 3. by diet
+    # 4. by cuisine
+    # 5. by ingredients
+
+    max_time = 2925
+    min_time = 0
+    max_serving = 1000
+    min_serving = 1
+
+    input_vector = []
+    # Normalizing Servings and Total time columns
+    input_vector.append((form.servings - min_serving) / (max_serving - min_serving))
+    input_vector.append((form.total_time - min_time) / (max_time - min_time))
+
+    input_vectors = [input_vector for i in range(5)]
+    
+    input_lists = [
+        form.ingredients + [form.course] + [form.diet] + [form.cuisine],
+        [form.course], 
+        [form.diet],
+        [form.cuisine],
+        form.ingredients,
+        ]
+    print(input_lists)
+    
+
+    for i, category in enumerate(['similar dish','course','diet','cuisine','ingredients']):
+
+        for col in encoded_df.columns[2:]:
+        # If the column is in the input list, set its value to 1, otherwise set it to 0
+            response = {}
+            input_vectors[i].append(int(col in input_lists[i]))   
+
+        similarity = cosine_similarity(encoded_df.to_numpy(), np.array(input_vectors[i]).reshape(1, -1))
+
+        sorted_indices = similarity.argsort(axis=0)[::-1][:5]
+
+        # Get the indices of the 5 most similar rows
+        most_similar_indices = sorted_indices.flatten()
+
+        recommendations = df.iloc[most_similar_indices]
+
+        response[category] = recommendations.to_dict(orient='index')
+    
+    return response
 
 @app.get('/recipe')
 def get_recipe(recipe: Recipe):
