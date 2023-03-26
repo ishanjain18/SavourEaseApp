be8918c9a0194bb34f6b6108b38e2ecd7622bd37
Ishan <ishan10j@gmail.com>
2023-03-27 04:20:09 +0530

    updated BASE_URL

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "http://localhost:8000";
 export const MIN_TIME = 0;
 export const MAX_TIME = 180;
 export const MIN_SERVINGS = 1;
